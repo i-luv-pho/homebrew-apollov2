1515ea727d3dddc478f94399a2a97bb976b2eee1
i-luv-pho <pho2610ana@proton.me>
2026-01-18 07:43:10 -0800

    Update to v1.0.1 with Apollo branding

--- Formula/apollo.rb
+++ Formula/apollo.rb
@@ -1,16 +1,16 @@
 class Apollo < Formula
   desc "AI-powered coding assistant for your terminal"
   homepage "https://github.com/i-luv-pho/apollov2"
-  version "1.0.0"
+  version "1.0.1"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/i-luv-pho/apollov2/releases/download/v1.0.0/apollo-darwin-arm64.tar.gz"
-      sha256 "5605e9089c68a46e07417a3c02c3d83d3a47cc358a6694ad8d328965c40b4e54"
+      url "https://github.com/i-luv-pho/apollov2/releases/download/v1.0.1/apollo-darwin-arm64.tar.gz"
+      sha256 "00a66b6c359a59c5171625fe94a09e7aa92ebd0d0ec5df81116f599405e87ce1"
     else
-      url "https://github.com/i-luv-pho/apollov2/releases/download/v1.0.0/apollo-darwin-x64.tar.gz"
-      sha256 "c859b91fd32f8898892bf4c00e84eddc4a05167c4a3b2ecf9d2507c8da7ac76d"
+      url "https://github.com/i-luv-pho/apollov2/releases/download/v1.0.1/apollo-darwin-x64.tar.gz"
+      sha256 "1482b36c2ba8633bdb8119eb6c9a5e4453870940f3797ce5c788c8a886a1c8ec"
     end
   end
 
